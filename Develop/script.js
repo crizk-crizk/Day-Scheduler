7afa0247816ebf2b71631bb02b79dd0e7459110e
Carolina R <carolinarizk@gmail.com>
2021-01-22 17:56:36 -0500

    saved to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -32,9 +32,25 @@ saveButtonElements.on("click", (event) => {
   var targetId = targetButton.id;
   //split targetId so only the number can be used
   var targetNumber = targetId.substring(4);
-  //display text of tarted textarea using targetNumber
-  console.log($("#text-" + targetNumber).val());
+  //local storage value
+  var localStorageText = $("#text-" + targetNumber).val();
+  //local storage key variable
+  var localStorageKey = "text-" + targetNumber;
+  //call helper function to save to storage
+  savetoLocalStorage(localStorageKey, localStorageText);
 });
 
-
-
+//Save to local storage 
+const savetoLocalStorage = (key, value) => {
+  if (localStorage.getItem('savedTasks')) {
+    //get array from local storage, parse it and store it in a variable.
+    let tasks = JSON.parse(localStorage.getItem('savedTasks'))
+    tasks[key]=value
+    localStorage.setItem('savedTasks', JSON.stringify(tasks));
+    //if there are no saved tasks yet, create tasks array and saved storage
+  } else {
+    let tasks = {[key]:value};
+    localStorage.setItem("savedTasks", JSON.stringify(tasks));
+  }
+};
+//helper func
